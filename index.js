5d9aaa1b733cba06ff6c79f4279564740e0e76e3
sudipto68 <sudiptomitro2016@gmail.com>
2021-04-02 21:54:47 +0600

    port changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const ObjectID = require("mongodb").ObjectID;
-const port = process.env.PORT || 5055;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
