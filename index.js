2e4f91423bd30f1c17d70ce43d97e060517e13b5
sudipto68 <sudiptomitro2016@gmail.com>
2021-04-21 01:30:32 +0600

    order query added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const ObjectID = require("mongodb").ObjectID;
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5055;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,11 +18,20 @@ const client = new MongoClient(uri, {
 client.connect((err) => {
   console.log(err);
   const productCollection = client.db("freshdb").collection("products");
+  const orderCollection = client.db("freshdb").collection("orders");
 
   app.get("/products", (req, res) => {
     productCollection.find().toArray((err, items) => {
       res.send(items);
     });
+  });
+
+  app.get("/orders", (req, res) => {
+    orderCollection
+      .find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
   });
 
   app.post("/addproducts", (req, res) => {
